3d23dfcdafdc11074bace11430513f8e02e53395
Masahiro Harada <masahiro.harada@ari-jp.com>
2018-12-02 01:57:24 +0900

    フォルダ作成機能

---
+++ resources/views/folders/create.blade.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>ToDo App</title>
+  <link rel="stylesheet" href="/css/styles.css">
+</head>
+<body>
+<header>
+  <nav class="my-navbar">
+    <a class="my-navbar-brand" href="/">ToDo App</a>
+  </nav>
+</header>
+<main>
+  <div class="container">
+    <div class="row">
+      <div class="col col-md-offset-3 col-md-6">
+        <nav class="panel panel-default">
+          <div class="panel-heading">フォルダを追加する</div>
+          <div class="panel-body">
+            @if($errors->any())
+              <div class="alert alert-danger">
+                @foreach($errors->all() as $message)
+                  <p>{{ $message }}</p>
+                @endforeach
+              </div>
+            @endif
+            <form action="{{ route('folders.create') }}" method="post">
+              @csrf
+              <div class="form-group">
+                <label for="title">フォルダ名</label>
+                <input type="text" class="form-control" name="title" id="title" value="{{ old('title') }}" />
+              </div>
+              <div class="text-right">
+                <button type="submit" class="btn btn-primary">送信</button>
+              </div>
+            </form>
+          </div>
+        </nav>
+      </div>
+    </div>
+  </div>
+</main>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,3 +1,6 @@
 <?php
 
 Route::get('/folders/{id}/tasks', 'TaskController@index')->name('tasks.index');
+
+Route::get('/folders/create', 'FolderController@showCreateForm')->name('folders.create');
+Route::post('/folders/create', 'FolderController@create');
