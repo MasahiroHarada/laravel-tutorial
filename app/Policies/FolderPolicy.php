fef7774387f782a33e702f8524472973a6d4072a
Masahiro Harada <masahiro.harada@ari-jp.com>
2018-12-06 04:35:20 +0900

    エラーハンドリング

---
+++ app/Policies/FolderPolicy.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Policies;
+
+use App\Folder;
+use App\User;
+
+class FolderPolicy
+{
+    /**
+     * フォルダの閲覧権限
+     * @param User $user
+     * @param Folder $folder
+     * @return bool
+     */
+    public function view(User $user, Folder $folder)
+    {
+        return $user->id === $folder->user_id;
+    }
+}
